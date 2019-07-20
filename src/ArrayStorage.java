5ea329a14787f922d9e22a3c16bdfd96709902a3
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-07-20 18:06:13 +0300

    add update() method, change some methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import static java.util.Arrays.copyOfRange;
+import static java.util.Arrays.fill;
+
 /**
  * Array based storage for Resumes
  */
@@ -6,17 +9,40 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
-        while (size > 0) {
-            storage[size - 1] = null;
-            size--;
+    void update(Resume original, Resume updateResume) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(original)) {
+                storage[i].uuid = updateResume.uuid;
+            }
         }
     }
 
+    void clear() {
+        fill(storage, null);
+        size = 0;
+    }
+
     void save(Resume r) {
-        if (r != null) {
-            storage[size] = r;
-            size++;
+        boolean resumeExist = false;
+        if (r != null && size < storage.length - 1) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].equals(r)) {
+                    resumeExist = true;
+                    break;
+                }
+            }
+            if (resumeExist == false) {
+                storage[size] = r;
+                size++;
+            }
+        } else {
+            System.out.print("Resume \"" + r + "\" doesn't save");
+            if (r == null) {
+                System.out.println(" because resume can't be \"null\"");
+            }
+            if (size >= storage.length - 1) {
+                System.out.println(" not enough free cells in massive");
+            }
         }
     }
 
@@ -26,38 +52,25 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
+        System.out.println("Resume \"" + uuid + "\" doesn't found in massive");
         return null;
     }
 
     void delete(String uuid) {
-        int deleteIndex = -1;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                deleteIndex = i;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
-        if (deleteIndex != -1) {
-            for (int i = deleteIndex; i < size; i++) {
-                Resume tmp = storage[i];
-                storage[i] = storage[i + 1];
-                storage[i + 1] = tmp;
-            }
-            size--;
-        }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] filteredResume = new Resume[size];
-
-        for (int i = 0; i < size; i++) {
-            filteredResume[i] = storage[i];
-        }
-        return filteredResume;
+        return copyOfRange(storage, 0, size);
     }
 
     int size() {
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,7 +2,7 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -11,10 +11,14 @@ public class MainTestArrayStorage {
         r2.uuid = "uuid2";
         Resume r3 = new Resume();
         r3.uuid = "uuid3";
+        Resume r4 = new Resume();
+        r4.uuid = "uuid4";
+        Resume r5 = null;
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r5);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
@@ -24,13 +28,15 @@ public class MainTestArrayStorage {
         printAll();
         ARRAY_STORAGE.delete(r1.uuid);
         printAll();
+        ARRAY_STORAGE.update(r2,r4);
+        System.out.println("Test update method, expect: uuid4 Actual: " + ARRAY_STORAGE.get(r2.uuid));
         ARRAY_STORAGE.clear();
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
-    static void printAll() {
+    private static void printAll() {
         System.out.println("\nGet All");
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
