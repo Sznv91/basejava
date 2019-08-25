7782f271110f7a7ab1190ce7d568da16d5ea48be
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-08-25 18:17:59 +0300

    HW4. make invoke methods via reflection

---
+++ src/ru/topjava/basejava/MainReflection.java
@@ -0,0 +1,25 @@
+package ru.topjava.basejava;
+
+import ru.topjava.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Class clazz = r.getClass();
+        System.out.println(Arrays.toString(clazz.getDeclaredMethods()));
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        field.set(r, "new_uuid");
+        // TODO : invoke r.toString via reflection
+        Method toString = clazz.getDeclaredMethod("toString");
+        System.out.println("Reflection invoke " + toString.invoke(r));
+
+        System.out.println(r);
+    }
+}
