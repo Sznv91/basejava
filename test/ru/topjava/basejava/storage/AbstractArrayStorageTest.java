eae13826e14a7e874895f23306aa9a6604873116
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-08-29 19:25:01 +0300

    HW4. fix TestClass

--- test/ru/topjava/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/topjava/basejava/storage/AbstractArrayStorageTest.java
@@ -13,43 +13,46 @@ abstract class AbstractArrayStorageTest {
 
     private final Storage storage;
 
-    AbstractArrayStorageTest(Storage storage) {
+    private static final int STORAGE_LIMIT = 10000;
+    private static final String UUID_1 = "UUID_1";
+    private static final String UUID_2 = "UUID_2";
+    private static final String UUID_3 = "UUID_3";
+    private static final String UUID_4 = "UUID_4";
+    private static final String UUID_5 = "UUID_5";
+    private static final Resume R_1 = new Resume(UUID_1);
+    private static final Resume R_2 = new Resume(UUID_2);
+    private static final Resume R_3 = new Resume(UUID_3);
+    private static final Resume R_4 = new Resume(UUID_4);
+    private static final Resume R_5 = new Resume(UUID_5);
+
+    protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
-
-    private static final String uuid1 = "uuid1";
-    private static final Resume r1 = new Resume(uuid1);
-    private static final String uuid2 = "uuid2";
-    private static final Resume r2 = new Resume(uuid2);
-    private static final String uuid3 = "uuid3";
-    private static final Resume r3 = new Resume(uuid3);
-    private static final String uuid4 = "uuid4";
-    private static final Resume r4 = new Resume(uuid4);
 
     @BeforeEach
     void setUp() {
         storage.clear();
-        storage.save(r1);
-        storage.save(r2);
-        storage.save(r3);
-        storage.save(r4);
+        storage.save(R_1);
+        storage.save(R_2);
+        storage.save(R_3);
+        storage.save(R_4);
     }
 
     @Test
     void save() {
-        storage.save(new Resume("uuid5"));
+        storage.save(R_5);
         assertEquals(5, storage.size());
-        assertEquals(storage.get("uuid5"), new Resume("uuid5"));
+        assertEquals(R_5, storage.get(UUID_5));
     }
 
     @Test
-    void saveDuplicate() {
-        assertThrows(ExistStorageException.class, () -> storage.save(new Resume(uuid1)));
+    void saveExist() {
+        assertThrows(ExistStorageException.class, () -> storage.save(new Resume(UUID_1)));
     }
 
     @Test
     void saveOverflow() {
-        while (storage.size() < 10_000) {
+        while (storage.size() < STORAGE_LIMIT) {
             storage.save(new Resume());
         }
         assertThrows(StorageException.class, () -> storage.save(new Resume()));
@@ -57,26 +60,34 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void delete() {
-        storage.delete("uuid1");
-        assertThrows(NotExistStorageException.class, () -> storage.get("uuid1"));
+        storage.delete(UUID_1);
+        assertThrows(NotExistStorageException.class, () -> storage.get(UUID_1));
         assertEquals(3, storage.size());
     }
 
     @Test
+    void deleteNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.delete(UUID_5));
+    }
+
+    @Test
     void update() {
-        Resume r5 = new Resume("uuid3");
-        storage.update(r5);
-        assertEquals(r5, storage.get("uuid3"));
+        Resume r6 = new Resume(UUID_3);
+        storage.update(r6);
+        assertEquals(r6, storage.get(UUID_3));
     }
 
     @Test
-    void getFirstElement() {
-        assertEquals(storage.get("uuid1"), new Resume(uuid1));
+    void updateNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.update(R_5));
     }
 
     @Test
-    void getLastElement() {
-        assertEquals(storage.get(uuid4), new Resume(uuid4));
+    void getElement() {
+        assertEquals(R_1, storage.get(UUID_1));
+        assertEquals(R_2, storage.get(UUID_2));
+        assertEquals(R_3, storage.get(UUID_3));
+        assertEquals(R_4, storage.get(UUID_4));
     }
 
     @Test
@@ -87,12 +98,13 @@ abstract class AbstractArrayStorageTest {
     @Test
     void getAll() {
         Resume[] expect = new Resume[4];
-        expect[0] = r1;
-        expect[1] = r2;
-        expect[2] = r3;
-        expect[3] = r4;
+        expect[0] = R_1;
+        expect[1] = R_2;
+        expect[2] = R_3;
+        expect[3] = R_4;
 
         assertArrayEquals(expect, storage.getAll());
+        assertEquals(4, storage.size());
     }
 
     @Test
