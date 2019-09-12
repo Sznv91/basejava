261a544d800f4b8518d1de22833cc097c568713e
Mihail Sazonov <sazonov91@gmail.com>
2019-09-12 23:41:23 +0300

    HW5. add array list and test

---
+++ src/ru/topjava/basejava/storage/ListStorage.java
@@ -0,0 +1,70 @@
+package ru.topjava.basejava.storage;
+
+import ru.topjava.basejava.exeption.ExistStorageException;
+import ru.topjava.basejava.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+
+    private final List<Resume> storage = new ArrayList<>();
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume resume = new Resume(uuid);
+        if (storage.contains(resume)) {
+            return storage.indexOf(resume);
+        }
+        return -1;
+    }
+
+    @Override
+    protected Resume doGet(int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    protected void doSave(int index, Resume resume) {
+
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected void doDelete(int index) {
+        storage.remove(index);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public void save(Resume resume) {
+        if (storage.contains(resume)) {
+            throw new ExistStorageException(resume.getUuid());
+        } else {
+            storage.add(resume);
+        }
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        Resume[] massive = new Resume[storage.size()];
+        for (int i = 0; i < storage.size(); i ++){
+            massive [i] = storage.get(i);
+        }
+        return massive;
+    }
+}
---
+++ test/ru/topjava/basejava/storage/ListStorageTest.java
@@ -0,0 +1,106 @@
+package ru.topjava.basejava.storage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.topjava.basejava.exeption.ExistStorageException;
+import ru.topjava.basejava.exeption.NotExistStorageException;
+import ru.topjava.basejava.model.Resume;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListStorageTest {
+
+    private final Storage storage = new ListStorage();
+
+    private static final String UUID_1 = "UUID_1";
+    private static final String UUID_2 = "UUID_2";
+    private static final String UUID_3 = "UUID_3";
+    private static final String UUID_4 = "UUID_4";
+    private static final String UUID_5 = "UUID_5";
+    private static final Resume R_1 = new Resume(UUID_1);
+    private static final Resume R_2 = new Resume(UUID_2);
+    private static final Resume R_3 = new Resume(UUID_3);
+    private static final Resume R_4 = new Resume(UUID_4);
+    private static final Resume R_5 = new Resume(UUID_5);
+
+    @BeforeEach
+    void setUp() {
+        storage.clear();
+        storage.save(R_1);
+        storage.save(R_2);
+        storage.save(R_3);
+        storage.save(R_4);
+    }
+
+    @Test
+    void save() {
+        storage.save(R_5);
+        assertEquals(5, storage.size());
+        assertEquals(R_5, storage.get(UUID_5));
+    }
+
+    @Test
+    void saveExist() {
+        assertThrows(ExistStorageException.class, () -> storage.save(new Resume(UUID_1)));
+    }
+
+    @Test
+    void delete() {
+        storage.delete(UUID_1);
+        assertThrows(NotExistStorageException.class, () -> storage.get(UUID_1));
+        assertEquals(3, storage.size());
+    }
+
+    @Test
+    void deleteNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.delete(UUID_5));
+    }
+
+    @Test
+    void update() {
+        Resume r6 = new Resume(UUID_3);
+        storage.update(r6);
+        assertEquals(r6, storage.get(UUID_3));
+    }
+
+    @Test
+    void updateNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.update(R_5));
+    }
+
+    @Test
+    void getElement() {
+        assertEquals(R_1, storage.get(UUID_1));
+        assertEquals(R_2, storage.get(UUID_2));
+        assertEquals(R_3, storage.get(UUID_3));
+        assertEquals(R_4, storage.get(UUID_4));
+    }
+
+    @Test
+    void getNotExist() {
+        assertThrows(NotExistStorageException.class, () -> storage.get("uuid-1"));
+    }
+
+    @Test
+    void getAll() {
+        Resume[] expect = new Resume[4];
+        expect[0] = R_1;
+        expect[1] = R_2;
+        expect[2] = R_3;
+        expect[3] = R_4;
+
+        assertArrayEquals(expect, storage.getAll());
+        assertEquals(4, storage.size());
+    }
+
+    @Test
+    void clear() {
+        storage.clear();
+        assertEquals(0, storage.size());
+    }
+
+    @Test
+    void size() {
+        assertEquals(4, storage.size());
+    }
+}
