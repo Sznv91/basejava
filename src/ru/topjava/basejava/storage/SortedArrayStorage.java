c9b6d19ff930e35f5d689dff6a5059e0423fb8a0
Mihail Sazonov <sazonov91@gmail.com>
2019-09-18 22:42:04 +0300

    HW6. Removed extra brackets in SortedArrayStorage

--- src/ru/topjava/basejava/storage/SortedArrayStorage.java
+++ src/ru/topjava/basejava/storage/SortedArrayStorage.java
@@ -31,16 +31,16 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void pasteResume(int index, Resume resume) {
-        int position = index * (-1) - 1;
+        int position = -index - 1;
         if (position != size) {
-            System.arraycopy(storage, position, storage, position + 1, (size - position));
+            System.arraycopy(storage, position, storage, position + 1, size - position);
         }
         storage[position] = resume;
     }
 
     @Override
     protected void deleteFromArray(int index) {
-        System.arraycopy(storage, index + 1, storage, index, (size - index) - 1);
+        System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
     }
 
     @Override
