a1205fb522928c969e92b63bb0a7a56d527c59ad
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-09-23 12:54:36 +0300

    HW6. Change getAllSorted method in AbstractStorage

--- src/ru/topjava/basejava/storage/AbstractStorage.java
+++ src/ru/topjava/basejava/storage/AbstractStorage.java
@@ -4,7 +4,6 @@ import ru.topjava.basejava.exeption.ExistStorageException;
 import ru.topjava.basejava.exeption.NotExistStorageException;
 import ru.topjava.basejava.model.Resume;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -66,18 +65,23 @@ public abstract class AbstractStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
-        List<Resume> result = doGetAllSorted();
-        //http://qaru.site/questions/68187/how-to-sort-by-two-fields-in-java
-        Collections.sort(result, new Comparator<Resume>() {
+        class ResumeNameComparator implements Comparator<Resume> {
+
             @Override
             public int compare(Resume o1, Resume o2) {
-                if (o1.getFullName().compareTo(o2.getFullName()) != 0) {
-                    return o1.getFullName().compareTo(o2.getFullName());
-                } else {
-                    return o1.getUuid().compareTo(o2.getUuid());
-                }
+                return o1.getFullName().compareTo(o2.getFullName());
             }
-        });
+        }
+        class ResumeUUIDComparator implements Comparator<Resume> {
+
+            @Override
+            public int compare(Resume o1, Resume o2) {
+                return o1.getUuid().compareTo(o2.getUuid());
+            }
+        }
+        List<Resume> result = doGetAllSorted();
+        //http://qaru.site/questions/68187/how-to-sort-by-two-fields-in-java
+        result.sort(new ResumeNameComparator().thenComparing(new ResumeUUIDComparator()));
         return result;
     }
 
