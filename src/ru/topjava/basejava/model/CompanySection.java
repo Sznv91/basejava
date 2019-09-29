060859b0ac4bf10b378313bc89079c256a54fb1e
Mihail Sazonov <sazonov91@gmail.com>
2019-09-29 15:44:48 +0300

    HW7. Change company Section

--- src/ru/topjava/basejava/model/CompanySection.java
+++ src/ru/topjava/basejava/model/CompanySection.java
@@ -1,7 +1,15 @@
 package ru.topjava.basejava.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CompanySection extends Section {
-    private ArrayList<Company> company = new ArrayList<>();
+    private Map<String, Company> companies = new HashMap<>();
+
+    public void addCompany(String companyName, Company company){
+        companies.put(companyName,company);
+    }
+    public Company getCompany(String companyName){
+        return companies.get(companyName);
+    }
 }
