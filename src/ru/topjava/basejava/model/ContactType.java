eb3ead5692b2812ac8bfc033b00909830357140c
Mihail Sazonov <sazonov91@gmail.com>
2019-10-07 22:14:58 +0300

    HW7. add description to fields in ContactType class

--- src/ru/topjava/basejava/model/ContactType.java
+++ src/ru/topjava/basejava/model/ContactType.java
@@ -1,12 +1,22 @@
 package ru.topjava.basejava.model;
 
 public enum ContactType {
-    FULL_NAME,
-    PHONE,
-    SKYPE,
-    EMAIL,
-    LINKEDIN,
-    GITHUB,
-    STACKOVERFLOW,
-    HOMEPAGE;
+    FULL_NAME("Полное имя"),
+    PHONE("Телефон"),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    LINKEDIN("Профиль LinkedIn"),
+    GITHUB("Профиль GitHub"),
+    STACKOVERFLOW("Профиль Stackoverflow"),
+    HOMEPAGE("Домашняя страница");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
