2e44b705819b1525b8624148f45d9052d20a5c93
Mihail Sazonov <sazonov91@gmail.com>
2019-12-09 09:43:44 +0300

    HW10. Close resources Input/Output with "try()"

--- src/ru/topjava/basejava/storage/objectStreamStorage/DataSerializeStrategyStorage.java
+++ src/ru/topjava/basejava/storage/objectStreamStorage/DataSerializeStrategyStorage.java
@@ -14,62 +14,61 @@ public class DataSerializeStrategyStorage implements StorageStrategy {
     public void doWrite(Resume r, OutputStream outputStream) throws IOException {
         int contactCount = r.getContacts().size();
         int sectionCount = r.getSections().size();
+        try (DataOutputStream dos = new DataOutputStream(outputStream)) {
+            dos.writeUTF(r.getUuid());
+            dos.writeUTF(r.getFullName());
+            dos.writeInt(contactCount);
 
-        DataOutputStream dos = new DataOutputStream(outputStream);
-        dos.writeUTF(r.getUuid());
-        dos.writeUTF(r.getFullName());
-        dos.writeInt(contactCount);
+            for (Map.Entry<ContactType, String> entry : r.getContacts().entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
+            }
+            dos.writeInt(sectionCount);
 
-        for (Map.Entry<ContactType, String> entry : r.getContacts().entrySet()) {
-            dos.writeUTF(entry.getKey().name());
-            dos.writeUTF(entry.getValue());
-        }
-        dos.writeInt(sectionCount);
-
-        for (Map.Entry<SectionType, AbstractSection> entry : r.getSections().entrySet()) {
-            switch (entry.getKey().ordinal()) {
-                case 0:
-                case 1:
-                    dos.writeUTF(String.valueOf(entry.getValue()));
-                    break;
-                case 2:
-                case 3:
-                    writeListSection(dos, (ListSection) r.getSection(entry.getKey()));
-                    break;
-                case 4:
-                case 5:
-                    writeCompanySections(dos, ((CompanySection) r.getSection(entry.getKey())).getCompanies());
-                    break;
-                default:
-                    break;
+            for (Map.Entry<SectionType, AbstractSection> entry : r.getSections().entrySet()) {
+                switch (entry.getKey().ordinal()) {
+                    case 0:
+                    case 1:
+                        dos.writeUTF(String.valueOf(entry.getValue()));
+                        break;
+                    case 2:
+                    case 3:
+                        writeListSection(dos, (ListSection) r.getSection(entry.getKey()));
+                        break;
+                    case 4:
+                    case 5:
+                        writeCompanySections(dos, ((CompanySection) r.getSection(entry.getKey())).getCompanies());
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        dos.close();
     }
 
     @Override
     public Resume doRead(InputStream inputStream) throws IOException {
         Resume result;
-        DataInputStream dis = new DataInputStream(inputStream);
-        String uuid = dis.readUTF();
-        String fullName = dis.readUTF();
-        result = new Resume(uuid, fullName);
-        int contactCount = dis.readInt();
+        try (DataInputStream dis = new DataInputStream(inputStream)) {
+            String uuid = dis.readUTF();
+            String fullName = dis.readUTF();
+            result = new Resume(uuid, fullName);
+            int contactCount = dis.readInt();
 
-        for (int i = 0; i < contactCount; i++) {
-            result.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-        }
+            for (int i = 0; i < contactCount; i++) {
+                result.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+            }
 
-        int sectionCounter = dis.readInt();
-        if (sectionCounter > 0) {
-            result.setSection(SectionType.PERSONAL, new TextSection(dis.readUTF()));
-            result.setSection(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
-            result.setSection(SectionType.ACHIEVEMENT, readListSection(dis));
-            result.setSection(SectionType.QUALIFICATIONS, readListSection(dis));
-            result.setSection(SectionType.EXPERIENCE, readCompanySection(dis));
-            result.setSection(SectionType.EDUCATION, readCompanySection(dis));
+            int sectionCounter = dis.readInt();
+            if (sectionCounter > 0) {
+                result.setSection(SectionType.PERSONAL, new TextSection(dis.readUTF()));
+                result.setSection(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
+                result.setSection(SectionType.ACHIEVEMENT, readListSection(dis));
+                result.setSection(SectionType.QUALIFICATIONS, readListSection(dis));
+                result.setSection(SectionType.EXPERIENCE, readCompanySection(dis));
+                result.setSection(SectionType.EDUCATION, readCompanySection(dis));
+            }
         }
-        dis.close();
         return result;
     }
 
