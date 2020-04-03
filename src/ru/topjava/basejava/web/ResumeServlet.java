affb76afa963aa9e490b2a952769bf0128126ba0
Mihail_Sazonov <sazonov91@gmail.com>
2020-04-03 09:25:56 +0300

    HW16. Add to servlet: shows contacts table for every resume

--- src/ru/topjava/basejava/web/ResumeServlet.java
+++ src/ru/topjava/basejava/web/ResumeServlet.java
@@ -1,6 +1,7 @@
 package ru.topjava.basejava.web;
 
 import ru.topjava.basejava.Config;
+import ru.topjava.basejava.model.ContactType;
 import ru.topjava.basejava.model.Resume;
 import ru.topjava.basejava.storage.Storage;
 
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
 
@@ -31,12 +33,18 @@ public class ResumeServlet extends javax.servlet.http.HttpServlet {
                 List<Resume> resumeList = storage.getAllSorted();
                 response.getWriter().println("<table cellpadding=\"5\" border=\"5\" style=\"border-collapse:collapse\" bgcolor=\"#a9a9a9\"><tr bgcolor=\"red\"><td>UUID:</td><td>Full Name:</td></tr>");
                 for (Resume resume : resumeList) {
-                    response.getWriter().println("<tr><td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td></tr>");
+                    response.getWriter().println("<tr><td>" + resume.getUuid() + "</td><td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "<a></td></tr>");
                 }
                 response.getWriter().println("</table>");
             } else {
                 Resume resume = storage.get(uuid);
                 response.getWriter().println("Hi " + resume.getFullName() + "!");
+                response.getWriter().write("<table border=\"1\" bgcolor=\"#a9a9a9\">" +
+                        "\n<tr bgcolor=\"red\"><td>Контакт</td><td>Значение</td></tr>");
+                for (Map.Entry<ContactType, String> contact : resume.getContacts().entrySet()) {
+                    response.getWriter().write("\n<tr> <td>" + contact.getKey().getTitle() + "</td> <td>" + contact.getValue() + "</td> </tr>");
+                }
+                response.getWriter().write("\n</table>");
             }
             response.getWriter().println("<br><a href=\"resume\">go back<a>");
         }
